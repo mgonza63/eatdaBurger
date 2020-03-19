f2a0c6700202415c45166f9f190d3994eca2c365
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-03-19 15:29:03 -0500

    installed packages needed and created data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devour)
+VALUES ("Big Burger", true);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ("Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ("Chicken Burger", true);
