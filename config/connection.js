a1bc76222c459645ff32c2d34490af583917aa43
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-03-21 16:10:59 -0500

    heroku fixes

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,7 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
+      console.log(err);
       return;
     }
     console.log("connected as id " + connection.threadId);
